54216e6b484958e3dc277ffb96aafc4776b12928
Marnix <marnixelling24@gmail.com>
2019-12-07 21:30:38 +0100

    completed options animation

--- www/animations.js
+++ www/animations.js
@@ -5,4 +5,11 @@ $(document).ready(function(){
     $(".settings-open").click(function(){
         $(".settings-menu").removeClass("opened-settings");
     });
+
+    $(".options-closed").click(function(){
+        $(".options-menu").addClass("opened-options");
+    });
+    $(".options-open").click(function(){
+        $(".options-menu").removeClass("opened-options");
+    });
 });
