1be48668ae62690d0d522c0f2af197804491681f
Marnix <marnixelling24@gmail.com>
2019-12-07 21:16:37 +0100

    finished settings animatio

--- www/animations.js
+++ www/animations.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(".settings-closed").click(function(){
-        $(".settings-options").addClass("opened-settings");
+        $(".settings-menu").addClass("opened-settings");
     });
     $(".settings-open").click(function(){
         $(".settings-menu").removeClass("opened-settings");
